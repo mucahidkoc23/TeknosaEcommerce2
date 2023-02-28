b95bb8452dcbf4c27087071c025ffdf4b6103ef0
silverback23 <cahid23@hotmail.com>
2023-02-28 10:47:51 +0300

    fixed problems 2

--- src/Redux/Provider.js
+++ src/Redux/Provider.js
@@ -1,14 +1,11 @@
 import React from "react";
 import {Provider} from "react-redux";
 import {legacy_createStore as createStore} from "redux";
-
 import Reducer from "./Reducer";
 import InitialState from "./Store";
 
 const UserProduct = ({children}) =>{
-  
   const store = createStore(Reducer,InitialState);
-
-  return <Provider store={store}>{children}</Provider>
+    return <Provider store={store}>{children}</Provider>
 }
 export default UserProduct;
